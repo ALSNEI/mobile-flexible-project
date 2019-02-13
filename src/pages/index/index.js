e0c6581a3f62a7fd963235e9827a7851089ea338
ALSNEI <1294609147@qq.com>
2019-02-13 10:48:53 +0800

    initial commit

---
+++ src/pages/index/index.js
@@ -0,0 +1,29 @@
+import './index.css'
+import 'assets/css/reset.css'
+import 'assets/css/common.scss'
+import 'lib-flexible'
+function component(){
+    let element = document.createElement('div');
+    let btn = document.createElement('button');
+
+    element.innerHTML = ['hello','webpack'].toString();
+    element.classList.add('hello');
+
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = function(){
+        alert("btn clicked")
+        $('.hello').css({'color':'green'})
+    };
+
+    
+    (()=>{
+        console.log(222);
+    })()
+
+    element.appendChild(btn);
+
+    
+
+    return element;
+}
+document.body.appendChild(component());
